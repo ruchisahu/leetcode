63e34ec41b809026630e3a93a2feec194c1c9f31
ruchisahu <sahruchi@gmail.com>
2018-05-08 22:29:35 -0700

    05/08/2018

---
+++ missing_Number.java
@@ -0,0 +1,25 @@
+/*268. Missing Number
+  Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+
+Example 1
+
+Input: [3,0,1]
+Output: 2
+
+Example 2
+
+Input: [9,6,4,2,3,5,7,0,1]
+Output: 8
+*/
+class  missing_Number {
+    public static int missingNumber(int[] nums) 
+    {
+        return 1;
+    }
+    
+     public static void main(String[] arg)
+    {
+        int[] nums={5,4,3,2,1};
+        System.out.println(missingNumber(nums) );
+    }
+}
