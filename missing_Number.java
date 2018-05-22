4b0421a6acabd2c4d7c8585bdc25222ca3d01f5e
Ruchi Sahu <sahruchi@gmail.com>
2018-05-22 12:55:27 -0700

    Add files via upload

--- missing_Number.java
+++ missing_Number.java
@@ -11,15 +11,45 @@ Example 2
 Input: [9,6,4,2,3,5,7,0,1]
 Output: 8
 */
+<<<<<<< HEAD
+import java.util.*;
+class  missing_Number {
+    public static int missingNumber(int[] nums) 
+    {
+        Arrays.sort(nums);
+        if (nums[nums.length-1] != nums.length)
+        {
+            return nums.length;
+        }
+         else if (nums[0] != 0) {
+            return 0;
+         }
+        int expect=nums[0];
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==expect)
+            {
+                expect=expect+1;
+            }
+            else
+                return expect;
+        }
+        return -1;
+=======
 class  missing_Number {
     public static int missingNumber(int[] nums) 
     {
         return 1;
+>>>>>>> 02c1c2c0c2cd7485d12eb48062ab313a125ed0f7
     }
     
      public static void main(String[] arg)
     {
+<<<<<<< HEAD
+        int[] nums={5,4,3,2,1,7};
+=======
         int[] nums={5,4,3,2,1};
+>>>>>>> 02c1c2c0c2cd7485d12eb48062ab313a125ed0f7
         System.out.println(missingNumber(nums) );
     }
 }
